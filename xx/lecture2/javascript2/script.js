fbcb5a8b239585367ecca65aa7626e740724ba62
Adeseto Akinwe <adeakinwe@gmail.com>
2024-01-12 14:10:42 +0100

    intro to js

---
+++ xx/lecture2/javascript2/script.js
@@ -0,0 +1,6 @@
+function changeToRed() {
+    document.getElementById('myDiv').innerHTML = "Change my colour to red";
+    document.getElementById('myDiv').style.color = 'red';
+    document.getElementById('myDiv').style.margin ='30px'
+    document.getElementById('myDiv').style.fontSize ='40px'
+}
