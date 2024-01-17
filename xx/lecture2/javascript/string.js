05f504fb19ee0619fe7b2e82450f3adfef0b7eb6
Adeseto Akinwe <adeakinwe@gmail.com>
2024-01-17 11:52:03 +0100

    strings

---
+++ xx/lecture2/javascript/string.js
@@ -0,0 +1,51 @@
+let comment = "Olaoluwa isn't very good at Mathematics";
+
+//string templates
+let text = `Hello Academy 4.0 students` //back-tic syntax
+
+let price = 150, quantity = 17;
+let totalCost = `Total cost of my goods is ${price * quantity}`; //template literal 
+console.log(totalCost);
+
+
+//typeof
+console.log(typeof(comment));
+console.log(typeof(price));
+
+//length
+console.log(comment.length)
+
+//escape
+let esc = "Academy 4.0 students are doing \"great\"";
+let escs = `Academy 4.0 students are doing \`great\``;
+console.log(esc);
+console.log(escs);
+
+//String Methods
+let states = "Ondo, Bauchi, Lagos, Kano";
+let state = states.slice(0,4);
+console.log(state);
+
+console.log(states.slice(14,19));
+
+console.log(states.substring(21,25));
+
+let state2 = states.slice(-11); //negative indexing
+console.log(state2);
+
+//replace
+let rep = 'I love Angular, Josphine loves Angular';
+let replaced = rep.replace('Angular', 'React').replace('Angular', 'Vue');
+console.log(replaced);
+
+//toUpperCase and toLowerCase
+let upper = "uppercase";
+console.log(upper.toUpperCase());
+
+let lower = "LOWERCASE";
+console.log(lower.toLowerCase());
+
+//concat
+let j = 'Good', k = 'Morning', l = 'Nigeria';
+let greeting = j.concat(' ', k, ' ', l);
+console.log(greeting);
---
+++ xx/lecture2/javascript2/variables.js
@@ -0,0 +1,23 @@
+//declaring a variable
+var x;
+var y,z;
+var sum;
+var X; //variables are case sensitive
+
+var y;
+//assigning variables
+x = 10, y = 15, z = 20;
+
+//statements
+sum = x + y;
+console.log("Sum =", sum);
+console.log(sum);
+
+console.log(X);
+
+//let
+let a; //does not allow re-declaration of a variable
+a = 2.5;
+
+//const 
+const b = 5; //does not allow re-declaration of a variable and non-assignment
